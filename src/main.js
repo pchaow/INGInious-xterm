27b1c9b9f99b4e15eb38fab64a8c0ab98effa5f9
Guillaume Derval <guillaume@guillaumederval.be>
2016-08-03 10:36:14 +0200

    Get info from query

--- src/main.js
+++ src/main.js
@@ -5,37 +5,47 @@ var term,
 
 var terminalContainer = document.getElementById('terminal-container');
 
-createTerminal("sirius", "22", "lol");
+createTerminal(getParameterByName("host"), getParameterByName("port"), getParameterByName("password"));
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return '';
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 function createTerminal(host, port, password) {
-  while (terminalContainer.children.length) {
-    terminalContainer.removeChild(terminalContainer.children[0]);
-  }
-  term = new Terminal({
-    cursorBlink: true
-  });
-  protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
-  socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + '/bash?host='+host+"&password="+password+"&port="+port;
-  socket = new WebSocket(socketURL);
+    while (terminalContainer.children.length) {
+        terminalContainer.removeChild(terminalContainer.children[0]);
+    }
+    term = new Terminal({
+        cursorBlink: true
+    });
+    protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
+    socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + '/bash?host=' + host + "&password=" + password + "&port=" + port;
+    socket = new WebSocket(socketURL);
 
-  term.open(terminalContainer);
-  term.fit();
+    term.open(terminalContainer);
+    term.fit();
 
-  socket.onopen = runRealTerminal;
-  socket.onclose = runFakeTerminal;
-  socket.onerror = runFakeTerminal;
+    socket.onopen = runRealTerminal;
+    socket.onclose = runFakeTerminal;
+    socket.onerror = runFakeTerminal;
 }
 
 
 function runRealTerminal() {
-  term.attach(socket);
-  term._initialized = true;
+    term.attach(socket);
+    term._initialized = true;
 }
 
 function runFakeTerminal() {
-  if (term._initialized) {
-    return;
-  }
-  term._initialized = true;
-  term.writeln('You are not allowed to access to this machine, or another error occured');
+    if (term._initialized) {
+        return;
+    }
+    term._initialized = true;
+    term.writeln('You are not allowed to access to this machine, or another error occured');
 }
